bf53d4152940b8da5529a066f7924252ca178aab
kaz <kizo.aria@protonmail.com>
2026-07-09 14:18:13 +0100

    fixup! wrong rule class name

--- checks/linux/ubuntu_24/initial_setup/process_hardening/Rule_1_5_5.py
+++ checks/linux/ubuntu_24/initial_setup/process_hardening/Rule_1_5_5.py
@@ -1,8 +1,8 @@
 from ._Base_1_5 import KernelParamRule
 
 
-class Rule_1_5_4(KernelParamRule):
-    rule_id = "1.5.4"
+class Rule_1_5_5(KernelParamRule):
+    rule_id = "1.5.5"
     _PARAM = "kernel.dmesg_restrict"
     _ALLOW = {"1"}
     title = f"Ensure {_PARAM} is configured"
