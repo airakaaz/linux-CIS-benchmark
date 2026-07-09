dfeaa949e35f4a34f4c059775add892cd16346d5
kaz <kizo.aria@protonmail.com>
2026-07-09 11:41:47 +0100

    add templates for verifying packages are not installed, installed and up to date

---
+++ checks/templates/verify_installed.py
@@ -0,0 +1,64 @@
+from core import CISRule, Mode, ScanResult
+from utils import package
+
+
+class VerifyInstalledRule(CISRule):
+    rule_id = ""
+    title = ""
+    mode = Mode.AUTOMATIC
+
+    _PACKAGES: set[str]
+    _VERIFY_UPTODATE: bool = False
+
+    def check(self) -> ScanResult:
+        installed = package.installed(*self._PACKAGES)
+
+        if installed.valid:
+            message = f"package(s) ({', '.join(self._PACKAGES)}) installed"
+        else:
+            message = f"package(s) ({', '.join(installed.anomalies)}) missing"
+
+        if not self._VERIFY_UPTODATE or not installed.valid:
+            return ScanResult(
+                rule_id=self.rule_id,
+                title=self.title,
+                passed=installed.valid,
+                message=message,
+            )
+        else:
+            uptodate = package.up_to_date(*self._PACKAGES)
+            if uptodate.valid:
+                message += f"; package(s) ({', '.join(self._PACKAGES)}) up to date"
+            else:
+                message += f"; package(s) ({', '.join(uptodate.anomalies)}) outdated"
+
+            return ScanResult(
+                rule_id=self.rule_id,
+                title=self.title,
+                passed=uptodate.valid,
+                message=message,
+            )
+
+
+class VerifyNotInstalledRule(CISRule):
+    rule_id = ""
+    title = ""
+    mode = Mode.AUTOMATIC
+
+    _PACKAGES: set[str]
+    _VERIFY_UPTODATE: bool = False
+
+    def check(self) -> ScanResult:
+        not_installed = package.not_installed(*self._PACKAGES)
+
+        if not_installed.valid:
+            message = f"package(s) ({', '.join(self._PACKAGES)}) not installed"
+        else:
+            message = f"package(s) ({', '.join(not_installed.anomalies)}) installed"
+
+        return ScanResult(
+            rule_id=self.rule_id,
+            title=self.title,
+            passed=not_installed.valid,
+            message=message,
+        )
--- utils/package.py
+++ utils/package.py
@@ -1,14 +1,38 @@
+from dataclasses import dataclass
+import re
+
 from utils.command import run
 
 
-def is_installed(package: str) -> bool:
-    return run(f"dpkg-query -W {package}").ok
+@dataclass(slots=True)
+class packageResult:
+    valid: bool
+    anomalies: set[str]
 
 
-def missing(*packages: str) -> list[str]:
-    missing: list[str] = []
+def installed(*packages: str) -> packageResult:
+    missing = set()
     for package in packages:
         if not run(f"dpkg-query -W {package}").ok:
-            missing.append(package)
+            missing.add(package)
 
-    return missing
+    return packageResult(valid=not missing, anomalies=missing)
+
+
+def up_to_date(*packages: str) -> packageResult:
+    outdated = set()
+    for package in packages:
+        result = run("apt list --upgradable")
+        if re.search(rf"^{package}\b", result.stdout):
+            outdated.add(package)
+
+    return packageResult(valid=not outdated, anomalies=outdated)
+
+
+def not_installed(*packages: str) -> packageResult:
+    installed = set()
+    for package in packages:
+        if run(f"dpkg-query -W {package}").ok:
+            installed.add(package)
+
+    return packageResult(valid=not installed, anomalies=installed)
