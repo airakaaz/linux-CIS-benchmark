2324173e383bc230374b9f5df0508889408776f8
kaz <kizo.aria@protonmail.com>
2026-07-09 11:42:26 +0100

    refactor rule 1.3.1.1 to use latest templates

--- checks/linux/ubuntu_24/initial_setup/apparmor/Rule_1_3_1_1.py
+++ checks/linux/ubuntu_24/initial_setup/apparmor/Rule_1_3_1_1.py
@@ -1,23 +1,8 @@
-from core import CISRule, Mode, ScanResult
-from utils import package
+from checks.templates.verify_installed import VerifyInstalledRule
 
 
-class Rule_1_3_1_1(CISRule):
+class Rule_1_3_1_1(VerifyInstalledRule):
     rule_id = "1.3.1.1"
     title = "Ensure apparmor packages are installed"
-    mode = Mode.AUTOMATIC
 
-    def check(self) -> ScanResult:
-        missing = package.missing("apparmor", "apparmor-utils")
-
-        if missing:
-            message = f"package(s) ({', '.join(missing)}) missing"
-        else:
-            message = "apparmor and apparmor-utils are installed"
-
-        return ScanResult(
-            rule_id=self.rule_id,
-            title=self.title,
-            passed=missing == [],
-            message=message,
-        )
+    _PACKAGES = {"apparmor", "apparmor-utils"}
