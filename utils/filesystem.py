7d2a2419d251508c3fc80efbe18153ec323f8a23
kaz <kizo.aria@protonmail.com>
2026-07-10 15:03:18 +0100

    add resolve_paths to filesystem utils

--- utils/filesystem.py
+++ utils/filesystem.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from glob import glob
 
 
 def exists(path: str) -> bool:
@@ -11,3 +12,12 @@ def read(path: str) -> str:
 
 def read_lines(path: str) -> list[str]:
     return read(path).splitlines()
+
+
+def resolve_paths(*patterns: str) -> list[str]:
+    paths = []
+
+    for pattern in patterns:
+        paths.extend(sorted(glob(pattern)))
+
+    return paths
