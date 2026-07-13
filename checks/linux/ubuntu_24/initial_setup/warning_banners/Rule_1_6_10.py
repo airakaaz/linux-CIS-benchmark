6d8c3a9195b12812bbe2c83db09e193221bdf308
kaz <kizo.aria@protonmail.com>
2026-07-13 10:18:19 +0100

    move get sshd banner to base for future use

--- checks/linux/ubuntu_24/initial_setup/warning_banners/Rule_1_6_10.py
+++ checks/linux/ubuntu_24/initial_setup/warning_banners/Rule_1_6_10.py
@@ -1,21 +1,4 @@
-from ._Base_1_6 import PathAccessRule
-from utils.command import run
-
-
-def get_sshd_banner() -> str:
-    result = run("sshd -T")
-
-    if not result.ok:
-        return ""
-
-    for line in result.stdout.splitlines():
-        if line.startswith("banner "):
-            banner = line.split(maxsplit=1)[1]
-            if banner.lower() == "none":
-                return ""
-            return banner
-
-    return ""
+from ._Base_1_6 import PathAccessRule, get_sshd_banner
 
 
 class Rule_1_6_10(PathAccessRule):
--- checks/linux/ubuntu_24/initial_setup/warning_banners/_Base_1_6.py
+++ checks/linux/ubuntu_24/initial_setup/warning_banners/_Base_1_6.py
@@ -1,1 +1,18 @@
 from checks.templates.path_access import PathAccessRule, MultiPathsAccessRule
+from utils.command import run
+
+
+def get_sshd_banner() -> str:
+    result = run("sshd -T")
+
+    if not result.ok:
+        return ""
+
+    for line in result.stdout.splitlines():
+        if line.startswith("banner "):
+            banner = line.split(maxsplit=1)[1]
+            if banner.lower() == "none":
+                return ""
+            return banner
+
+    return ""
