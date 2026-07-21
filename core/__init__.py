4a418fa720b923a5634e8ae660380ded55e5ff2f
kaz <kizo.aria@protonmail.com>
2026-07-21 10:36:37 +0100

    modify the ScanEngine class and remove registry

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,4 @@
 from .engine import ScanEngine
-from .registry import RuleRegistry
 from .result import ScanResult
 from .rule import CISRule, Mode
 from .module import Module
--- core/engine.py
+++ core/engine.py
@@ -1,44 +1,35 @@
-import re
-from core.registry import RuleRegistry
+from typing import Type
+from core.module import Module
 from core.result import ScanResult
-from core.rule import Mode
+from core.rule import CISRule, Mode
 
 
 class ScanEngine:
-    def __init__(self, registry: RuleRegistry):
-        self.registry = registry
+    def __init__(self):
+        self._modules: list[Module] = []
 
-    def _run(self, rules: list) -> list[ScanResult]:
+    def register(self, *modules: Module):
+        self._modules.extend(modules)
+
+    def run(self) -> list[ScanResult]:
         results: list[ScanResult] = []
         manual_checks = []
 
-        for rule in rules:
-            if rule.mode == Mode.AUTOMATIC:
-                try:
-                    results.append(rule().check())
-                except Exception as e:
-                    results.append(
-                        ScanResult(
-                            rule_id=rule.rule_id,
-                            title=rule.title,
-                            passed=False,
-                            message=f"check failed due to Exception: {e}",
+        for mod in self._modules:
+            for rule in mod.rules:
+                if rule.mode == Mode.AUTOMATIC:
+                    try:
+                        results.append(rule().check())
+                    except Exception as e:
+                        results.append(
+                            ScanResult(
+                                rule_id=rule.rule_id,
+                                title=rule.title,
+                                passed=False,
+                                message=f"check failed due to Exception: {e}",
+                            )
                         )
-                    )
-            else:
-                manual_checks.append(rule)
+                else:
+                    manual_checks.append(rule)
 
         return results
-
-    def run_all(self):
-        return self._run(self.registry.rules_list)
-
-    def run_matching(self, rule_id: str):
-        valid_id = re.compile(re.escape(rule_id))
-        rules = [
-            rule
-            for rule_id, rule in self.registry.rules_dict.items()
-            if valid_id.match(rule_id)
-        ]
-
-        return self._run(rules)
